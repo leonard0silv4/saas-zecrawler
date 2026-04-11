f0421f347b1b0493f3a4ed843689a33afaf3e4ce
Leonardo Silva <lorsilva.fcamara@rd.com.br>
2026-04-11 20:38:40 -0300

    add plans env

--- backend/config/plans.js
+++ backend/config/plans.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";                                                                                                                                                                                     
+dotenv.config();
+
+
 export const PLANS = {
   free: {
     name: "Gratuito",
